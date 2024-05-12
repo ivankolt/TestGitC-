8c473c6a299e063470f68931263e9f07ef3bc4c1
User <User@DESKTOP-2NV8R6V>
2024-05-12 11:11:43 +0500

    Добавьте файлы проекта.

---
+++ gitprogect/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitprogect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("привет мир");
+            while (true)
+            {
+                Console.WriteLine("пока");
+                    break;
+            }
+        }
+    }
+}
